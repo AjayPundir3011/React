407b044fe74d43507dc3790e4cc619690be6f50a
Ajay Pundir <ajay.pundir@uneecops.in>
2025-08-07 10:24:59 +0530

    new commit

--- vite project/src/App.jsx
+++ vite project/src/App.jsx
@@ -1,16 +1,33 @@
 import { useState } from 'react'
 import './App.css';
+import './index.css'
 import Card from './components/Card.jsx';
 
 
 function App() {
 
+  const [color, setColor] = useState("olive");
 
   return (
-    <>
-    <Card profile ="Chirag" imgText ="chirag image"/>
-    <Card profile ="Ashutosh" />
-    </>
+
+    <div
+    className="w-full h-screen duration-200 flex items-center justify-center"
+    style={{ backgroundColor: color }}
+  >
+    <h1 className="text-white text-3xl font-bold">Background is changing!</h1>
+  
+    <div className="fixed bottom-1 left-0 right-0 flex justify-center gap-4">
+      <button className="rounded-full bg-red-600 px-4 py-2 text-white shadow-lg"
+        style ={{background:"red"}} onClick={() => setColor("red")}>red</button>
+      <button className="rounded-full bg-red-600 px-4 py-2 text-white shadow-lg"
+        style ={{background:"green"}} onClick={() => setColor("green")}>green</button>
+        <button className="rounded-full bg-red-600 px-4 py-2 text-white shadow-lg"
+        style ={{background:"blue"}} onClick={() => setColor("blue")}>blue</button>
+    </div>
+  </div>
+  
+  
+
   )
 }
 
